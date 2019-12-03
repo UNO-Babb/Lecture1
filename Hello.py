634f3740bc8628f9d7c44d204873a34efbc6510e
Derek Babb <dbabb1@gmail.com>
2019-12-03 10:57:00 -0600

    Created Hello.py as our intro Python file.

---
+++ Hello.py
@@ -0,0 +1,11 @@
+#Name
+#Date
+#Project
+
+
+#We are going to make a function called main
+def main():
+    print("Hello world")
+
+#now that it is defined, we need to call our main function
+main()
